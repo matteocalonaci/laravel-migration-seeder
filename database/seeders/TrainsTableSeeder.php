64dd5db8b82e798529c8ef3958d6107da2788f76
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-07-03 22:18:33 +0200

    utilizzo i Faker per generare dari casuali

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,27 +5,31 @@ namespace Database\Seeders;
 use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+
+public function run(Faker $faker): void
     {
         for ($i=0; $i<10; $i++){
            $new_train = new Train();
-           $new_train->Azienda = 'Italo';
-           $new_train->Stazione_partenza = 'Firenze';
-           $new_train->Stazione_arrivo = 'Roma';
-           $new_train->Orario_partenza = '09:30:00';
-           $new_train->Orario_arrivo = '13:00:00';
-           $new_train->Codice_treno = '91 83 2464 062-5 I-TI';
-           $new_train->Numero_carreggiate = 10;
-           $new_train->In_orario = true;
-           $new_train->Cancellato  = false;
+           $new_train->Azienda = $faker->company();
+           $new_train->Stazione_partenza = $faker->city();
+           $new_train->Stazione_arrivo = $faker->city();
+           $new_train->Orario_partenza = $faker->time();
+           $new_train->Orario_arrivo = $faker->time();
+           $new_train->Codice_treno = $faker->ean13();
+           $new_train->Numero_carreggiate =$faker->numberBetween(5, 12);
+           $new_train->In_orario = $faker->numberBetween(0, 1);
+           $new_train->Cancellato  = $faker->numberBetween(0, 1);
            $new_train->save();
         }
 
     }
 }
+
+
